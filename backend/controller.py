c163708fe06f65a90ae163dc784cd45f749d345b
junyeong237 <junyeong237@gmail.com>
2024-06-02 22:49:41 +0900

    수정 페이지 수정

--- backend/controller.py
+++ backend/controller.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 
 
-password = 'temp'
+password = 'alalwl123!@#'
 encoded_password = quote_plus(password)
 
 engine = create_engine(f'mysql+pymysql://root:{encoded_password}@localhost/sprog')
@@ -22,12 +22,23 @@ session = DBSession()
 
 
 @app.route('/groups/<int:group_id>/list')
-def groupList(bookstore_id=None, group_id=None):
+def groupList(group_id=None):
     group = session.query(Group).filter_by(id=group_id).one()
     users = session.query(user_group).filter_by(group_id=group_id)
     checklist = session.query(CheckList).filter_by(group_id=group_id)
     return render_template(
         'group-details.html', group=group, user=users, checklist=checklist)
+
+@app.route('/groups/<int:group_id>')
+def groupDetail(group_id=None):
+    group = session.query(Group).filter_by(id=group_id).one()
+    user_ids = [user.user_id for user in session.query(user_group).filter_by(group_id=group_id).all()]
+    users = session.query(User).filter(Group.id.in_(user_ids)).all()
+    checklist = session.query(CheckList).filter_by(group_id=group_id)
+    print(checklist)
+    return render_template(
+        'scheduler.html', group=group, user=users, checklist=checklist)
+
 
 
 @app.route('/groups/<int:group_id>/new', methods=['GET', 'POST'])
@@ -39,12 +50,54 @@ def newGroupMem(bookstore_id):
 def newGroup():
     if request.method == 'POST':
         newGroup = Group(name=request.form['group-name'],description=request.form['group-info'])
-        print(newGroup)
         session.add(newGroup)
         session.commit()
-        return redirect(url_for('groupList', group_id=1))
+        tasks = request.form.getlist('task[]')
+
+        for task_content in tasks:
+            checkList = CheckList(content=task_content, group_id=newGroup.id,completed=False)
+            session.add(checkList)
+
+        session.commit()  # 모든 Task 객체 저장
+        return redirect(url_for('homeList'))
     else:
         return render_template('schedule-registration.html')
+
+
+@app.route('/groups/<int:group_id>/edit',
+           methods=['GET', 'POST'])
+def editGroup(group_id):
+    editedGroup = session.query(Group).filter_by(id=group_id).one()
+    editedTasks = session.query(CheckList).filter_by(group_id=group_id).all()
+    print(editedTasks)
+
+
+
+    if request.method == 'POST':
+        if request.form['group-name']:
+            editedGroup.name = request.form['group-name']
+        if request.form['group-info']:
+            editedGroup.description = request.form['group-info']
+
+        tasks = request.form.getlist('task[]')
+        completed = request.form.getlist('completed[]')
+
+        for task, task_content, is_completed in zip(editedTasks, tasks, completed):
+            checklist = session.query(CheckList).filter_by(id=task.id).one()
+            checklist.content = task_content
+            is_completed = True if is_completed == 'on' else False
+            checklist.completed = is_completed
+            session.add(checklist)
+        session.commit()
+
+
+        return redirect(url_for('groupList', group_id=group_id))
+    else:
+        return render_template(
+            'editgroup.html', group_id=group_id, group=editedGroup, checklist=editedTasks)
+
+
+
 
 @app.route('/')
 def homeList():
@@ -53,9 +106,24 @@ def homeList():
         'home.html', group=group)
 
 
+@app.route('/restaurants/<int:group_id>/<int:checklist_id>/delete',
+           methods=['POST'])
+def deleteCheckList(group_id, checklist_id):
+    checklist = session.query(CheckList).filter_by(id=checklist_id).one()
+    session.delete(checklist)
+    session.commit()
+    return redirect(url_for('homeList'))
+
+
 @app.route('/user/<int:user_id>')
-def userProfile():
-    return 0
+def userProfile(user_id=None):
+    user = session.query(User).filter_by(id=user_id).one()
+
+    group_ids = [group.group_id for group in session.query(user_group).filter_by(user_id=user_id).all()]
+    groups = session.query(Group).filter(Group.id.in_(group_ids)).all()
+    return render_template(
+        'profile.html', user=user, groups = groups)
+
 
 if __name__ == '__main__':
     app.debug = True
